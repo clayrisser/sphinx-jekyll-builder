e79dfd140ac7e7e8a366827d499ca0c837b019ac
Jam Risser <jam@codejam.ninja>
2018-11-14 16:15:31 -0600

    Finished initial working version

--- sphinx_jekyll_builder/jekyll_writer.py
+++ sphinx_jekyll_builder/jekyll_writer.py
@@ -1,7 +1,21 @@
 from sphinx_markdown_builder.markdown_writer import MarkdownTranslator, MarkdownWriter
+from munch import munchify
+import yaml
 
 class JekyllTranslator(MarkdownTranslator):
-    pass
+    def visit_document(self, node):
+        variables = munchify({
+            'current_docname': getattr(self.builder, 'current_docname'),
+            'images': getattr(self.builder, 'images'),
+            'versioning_method': getattr(self.builder, 'versioning_method')
+        })
+        variables_yaml = yaml.safe_dump(variables)
+        frontmatter = '---\n' + variables_yaml + '---\n'
+        self.add(frontmatter, section='head')
+        super().visit_document(node)
+
+    def depart_document(self, node):
+        super().depart_document(node)
 
 class JekyllWriter(MarkdownWriter):
     translator_class = JekyllTranslator
