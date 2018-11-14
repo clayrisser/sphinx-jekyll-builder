9c103a891b1fe185a3739bf9c068c61c8376f1ea
Jam Risser <jam@codejam.ninja>
2018-11-14 16:48:16 -0600

    Added title variable

--- sphinx_jekyll_builder/jekyll_writer.py
+++ sphinx_jekyll_builder/jekyll_writer.py
@@ -3,19 +3,33 @@ from munch import munchify
 import yaml
 
 class JekyllTranslator(MarkdownTranslator):
+    visited_title = False
+    title = None
+
     def visit_document(self, node):
+        self.title = getattr(self.builder, 'current_docname')
+        super().visit_document(node)
+
+    def depart_document(self, node):
         variables = munchify({
             'current_docname': getattr(self.builder, 'current_docname'),
             'images': getattr(self.builder, 'images'),
+            'title': self.title,
             'versioning_method': getattr(self.builder, 'versioning_method')
         })
         variables_yaml = yaml.safe_dump(variables)
         frontmatter = '---\n' + variables_yaml + '---\n'
         self.add(frontmatter, section='head')
-        super().visit_document(node)
-
-    def depart_document(self, node):
         super().depart_document(node)
+
+    def visit_title(self, node):
+        if not self.visited_title:
+            self.title = node.astext()
+        self.visited_title = True
+        super().visit_title(node)
+
+    def depart_title(self, node):
+        super().depart_title(node)
 
 class JekyllWriter(MarkdownWriter):
     translator_class = JekyllTranslator
